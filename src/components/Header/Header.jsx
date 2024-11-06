6ac3c4f59af8c9afe9809149a5dd8351abf45dae
Yilchik <yulyayulia2@gmail.com>
2024-11-06 21:49:31 +0100

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import { NavLink } from "react-router-dom";
 import css from "./Header.module.css";
 import clsx from "clsx";
-import logo from "../../assets/logo.svg";
 
 const buildLinkClass = ({ isActive }) => {
   return clsx(css.link, isActive && css.active);
@@ -11,15 +10,28 @@ const Header = () => (
   <div className={css.header}>
     <nav className={css.nav}>
       <NavLink to="/">
-        <img src={logo} alt="Logo" className={css.logo} />
+        <img src="/src/assets/logoDoloni.png" alt="Logo" className={css.logo} />
       </NavLink>
-      <div className={css.navigation}>
-        <NavLink to="/" className={buildLinkClass}>
-          Home
-        </NavLink>
-        <NavLink to="/catalog" className={buildLinkClass}>
-          Catalog
-        </NavLink>
+      <div className={css.headerText}>
+        <h1>Благодійний Фонд "Волонтерський рух Оберіг"</h1>
+        <p className={css.titel}>
+          "Тільки тим історія належить, хто сьогодні бореться й живе"{" "}
+          <span className={css.titelAvtor}>(В. Симоненко)</span>
+        </p>
+        <div className={css.navigation}>
+          <NavLink to="/" className={buildLinkClass}>
+            Головна
+          </NavLink>
+          <NavLink to="/catalog" className={buildLinkClass}>
+            Історія створення
+          </NavLink>
+          <NavLink to="/catalog" className={buildLinkClass}>
+            Діяльність фонду
+          </NavLink>
+          <NavLink to="/catalog" className={buildLinkClass}>
+            Як допомогти?
+          </NavLink>
+        </div>
       </div>
     </nav>
   </div>
