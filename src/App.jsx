cf46ff2296a6007809f96d3cf357a8495c53ff66
Yilchik <yulyayulia2@gmail.com>
2024-11-07 21:19:28 +0100

    page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
-import CatalogPage from "./pages/CatalogPage/CatalogPage";
+import HistoryPage from "./pages/HistoryPage/HistoryPage";
 import DetailsPage from "./pages/DetailsPage/DetailsPage";
 import Features from "./components/Features/Features";
 import Reviews from "./components/Reviews/Reviews";
@@ -9,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/catalog" element={<CatalogPage />} />
+      <Route path="/catalog" element={<HistoryPage />} />
       <Route path="/catalog/:id" element={<DetailsPage />}>
         <Route path="features" element={<Features />} />
         <Route path="reviews" element={<Reviews />} />
