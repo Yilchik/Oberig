14a74692521bc6b93563d9c0ec98f7e3bd723ad6
Yilchik <yulyayulia2@gmail.com>
2024-11-07 14:04:41 +0100

    background

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,4 @@
+import css from "./Footer.module.css";
+const Footer = () => <div className={css.footer}></div>;
+
+export default Footer;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,26 +1,22 @@
+import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
-import { useNavigate } from "react-router-dom";
 import css from "./HomePage.module.css";
 
 const HomePage = () => {
-  const navigate = useNavigate();
-
-  const handleViewNowClick = () => {
-    navigate("/catalog");
-  };
-
   return (
     <div>
       <Header />
       <div className={css.container}>
         <div className={css.title}>
-          <h1>Campers of your dreams</h1>
           <p className={css.text}>
-            You can find everything you want in our catalog
+            В умовах сучасних викликів, з якими стикається наша країна,
+            волонтерські організації стають важливими опорами для суспільства.
+            Однією з таких ініціатив є Благодійний фонд «Волонтерський Рух
+            Оберіг», що активно працює на благо українського народу.
           </p>
         </div>
-        <button onClick={handleViewNowClick}>View Now</button>
       </div>
+      <Footer />
     </div>
   );
 };
