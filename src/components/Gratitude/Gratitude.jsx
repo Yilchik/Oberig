120a044c7ada5d29a05f64f56ec385c50c6fa79d
Yilchik <yulyayulia2@gmail.com>
2024-12-05 20:04:25 +0100

    Images

--- src/components/Gratitude/Gratitude.jsx
+++ src/components/Gratitude/Gratitude.jsx
@@ -1,52 +1,68 @@
 import css from "./Gratitude.module.css";
-import medalImage from "../../assets/medal.jpg";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Images from "../Images/Images.jsx";
 
 const Gratitude = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => {
+  const [currentImage, setCurrentImage] = useState(null);
+
+  const openModal = (image) => {
+    setCurrentImage(image);
     setIsModalOpen(true);
     document.body.style.overflow = "hidden";
   };
 
   const closeModal = () => {
     setIsModalOpen(false);
+    setCurrentImage(null);
     document.body.style.overflow = "auto";
   };
 
-  const handleKeyDown = (e) => {
-    if (e.key === "Escape") {
-      closeModal();
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape") {
+        closeModal();
+      }
+    };
+
+    if (isModalOpen) {
+      window.addEventListener("keydown", handleKeyDown);
+    } else {
+      window.removeEventListener("keydown", handleKeyDown);
     }
-  };
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isModalOpen]);
 
   return (
-    <section className={css.container} onKeyDown={handleKeyDown} tabIndex={0}>
+    <section className={css.container}>
       <div className={css.gratitude}>
         <h2 className={css.title}>Подяки</h2>
         <ul className={css.gratitudeCard}>
-          <li className={css.gratitudePhoto}>
-            <img
-              src={medalImage}
-              alt="Фото Подяки Анушенковій Ганні"
-              width="264"
-              height="360"
-              loading="lazy"
-              onClick={openModal}
-              className={css.image}
-            />
-          </li>
+          {Images.map((image) => (
+            <li className={css.gratitudePhoto} key={image.id}>
+              <img
+                src={image.src}
+                alt={image.alt}
+                loading="lazy"
+                onClick={() => openModal(image)}
+                className={css.image}
+              />
+            </li>
+          ))}
         </ul>
       </div>
-      {isModalOpen && (
+      {isModalOpen && currentImage && (
         <div className={css.modalOverlay} onClick={closeModal}>
           <div
             className={css.modalContent}
             onClick={(e) => e.stopPropagation()}
           >
             <img
-              src={medalImage}
-              alt="Фото Подяки Анушенковій Ганні"
+              src={currentImage.src}
+              alt={currentImage.alt}
               className={css.modalImage}
             />
           </div>
