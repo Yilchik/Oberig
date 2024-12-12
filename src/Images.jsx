0d8f86631931523d2541f7f9cebcc54d14179f73
Yilchik <yulyayulia2@gmail.com>
2024-12-12 20:39:10 +0100

    Images

--- src/Images.jsx
+++ src/Images.jsx
@@ -1,13 +1,13 @@
 const Images = [
   { id: 1, src: "/assets/gr.jpg", alt: "Фото 1" },
-  { id: 2, src: "../src/assets/medal.jpg", alt: "Фото 2" },
-  { id: 3, src: "../src/assets/gr1.jpg", alt: "Фото 3" },
-  { id: 4, src: "../src/assets/gr2.jpg", alt: "Фото 4" },
-  { id: 5, src: "../src/assets/gr3.jpg", alt: "Фото 5" },
-  { id: 6, src: "../src/assets/gr4.jpg", alt: "Фото 6" },
-  { id: 7, src: "../src/assets/gr5.jpg", alt: "Фото 7" },
-  { id: 8, src: "../src/assets/gr6.jpg", alt: "Фото 8" },
-  { id: 9, src: "../src/assets/gr7.jpg", alt: "Фото 9" },
+  { id: 2, src: "/assets/medal.jpg", alt: "Фото 2" },
+  { id: 3, src: "/assets/gr1.jpg", alt: "Фото 3" },
+  { id: 4, src: "/assets/gr2.jpg", alt: "Фото 4" },
+  { id: 5, src: "/assets/gr3.jpg", alt: "Фото 5" },
+  { id: 6, src: "/assets/gr4.jpg", alt: "Фото 6" },
+  { id: 7, src: "/assets/gr5.jpg", alt: "Фото 7" },
+  { id: 8, src: "/assets/gr6.jpg", alt: "Фото 8" },
+  { id: 9, src: "/assets/gr7.jpg", alt: "Фото 9" },
 ];
 
 export default Images;
